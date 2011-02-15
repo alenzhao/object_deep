3052ef38d7b1042ed196c61bde2274fd83dc36a1
Mehdi MIRZA <mmirza@lucid64-workstation.idiap.ch>
2011-02-15 17:59:26 +0100

    prepare image working good

--- prepare_data.py
+++ prepare_data.py
@@ -2,7 +2,8 @@ import os
 import glob
 import scipy as sp
 import Image
-
+import pickle
+import argparse
 
 def get_image(img_fname, new_size = None, resize_method='bicubic'):
 	""" Return a grayscaled and resized image as numpy array
@@ -74,14 +75,15 @@ def get_image_list(img_path, im_size = None, class_list = None):
 					class_list]
 		# -- ToDo assert if list values are valid
 
-	# -- get all images
+	# -- get list of images
 	images = []
 	for item in class_list:
 		#get list
 		im_list = glob.glob(os.path.join(item, '*.jpg'))
 		im_list = sp.random.permutation(im_list)
 		
-		#get images as arrays
+		#get images as array or just check if the image is openable
+		#if im_size is none and return the file name
 		data = [get_image(name, im_size) for name in im_list]
 
 		#remove None values
@@ -122,57 +124,169 @@ def divide_data(images, im_n_limit, train_r, valid_r):
 	# -- intialize data
 	class_n = len(images)
 
-	train_data = [''] * (train_n * class_n)
-	valid_data = [''] * (valid_n * class_n)
-	test_data = [''] * (test_n * class_n)
+	train_data = []
+	valid_data = []
+	test_data = []
 
-	train_labels = sp.zeros((train_n * class_n, 1), dtype = int)
-	valid_labels = sp.zeros((valid_n * class_n, 1), dtype = int)
-	test_labels = sp.zeros((test_n * class_n, 1), dtype = int)
+	train_labels = []
+	valid_labels = []
+	test_labels = []
 	
 
 	# -- loop over all classes to divide data
 	for i_ind in range(class_n):
 		# -- divide data into train, validation and test
-		train_rng = xrange(i_ind * train_n, (i_ind + 1) * train_n)
-		valid_rng = xrange(i_ind * valid_n, (i_ind + 1) * valid_n)
-		test_rng = xrange(i_ind * test_n, (i_ind + 1) * test_n)
 
 		# data
-		train_data[train_rng] = images[i_ind][:train_n]
-		valid_data[valid_rng] = images[i_ind][train_n: train_n + valid_n]
-		test_data[test_rng] = images[i_ind][train_n + valid_n:]
+		train_data.extend(images[i_ind][:train_n])
+		valid_data.extend(images[i_ind][train_n: train_n + valid_n])
+		test_data.extend(images[i_ind][train_n + valid_n:im_n_limit])
 
 		# labels
-		train_lables[train_rng] = i_ind
-		valid_lables[valid_rng] = i_ind
-		test_lables[test_rn] = i_ind
+		train_labels.extend([i_ind] * train_n)
+		valid_labels.extend([i_ind] * valid_n)
+		test_labels.extend([i_ind] * test_n)
 
 	# -- final shuffle of data
-	shuffle_ind = sp.random.permutation(train_n * n_class)
-	train_data = train_data[shuffle_ind]
-	train_labels = train_labels[shuffle_ind]
+	shuffle_ind = sp.random.permutation(train_n * class_n)
+	train_data = sp.array(train_data)[shuffle_ind]
+	train_labels = sp.array(train_labels)[shuffle_ind]
 
-	shuffle_ind = sp.random.permutation(valid_n * n_class)
-	valid_data = valid_data[shuffle_ind]
-	train_labels = valid_labels[shuffle_ind]
+	shuffle_ind = sp.random.permutation(valid_n * class_n)
+	valid_data = sp.array(valid_data)[shuffle_ind]
+	valid_labels = sp.array(valid_labels)[shuffle_ind]
 	
-	shuffle_ind = sp.random.permutation(test_n * n_class)
-	test_data = test_data[shuffle_ind]
-	test_labels = test_labels[shuffle_ind]
+	shuffle_ind = sp.random.permutation(test_n * class_n)
+	test_data = sp.array(test_data)[shuffle_ind]
+	test_labels = sp.array(test_labels)[shuffle_ind]
+
 
 	return (train_data, train_labels), (valid_data, valid_labels),\
 			(test_data, test_labels)
 
 	
 
+def chunk_data(data, chunk_size, im_size, save_path, name_pattern):
+	"""Chunk the data and load images and save images data and
+		labels for each chunk"""
+
+	# -- preapre output path
+	if not os.path.isdir(save_path):
+		raise NameError('Invalid path: %s'% save_path)
+
+	# -- prepare data
+	data, labels = data
+	chunk_num = data.shape[0] / chunk_size
+
+	# -- loop over chunks, load data, pickle them
+	for i_ind in range(chunk_num):
+		images = data[i_ind*chunk_size:(i_ind+1) * chunk_size]
+		# --load image data
+		images = sp.array([get_image(name, im_size) for name in images])
+	
+		# --pickle
+		fname = os.path.join(save_path, "%s_%03d.pkl" % (name_pattern, i_ind))
+		output = open(fname , 'wb')
+		pickle.dump(images, output, -1)
+		pickle.dump(labels[i_ind*chunk_size:(i_ind) + 1 * chunk_size], 
+				output, -1)
+		output.close()
+
+		if verbose:
+			print "Pickled %s - %d/%d" % (fname, i_ind, chunk_num)
+
+def chunk_size(im_size, memory_size, data_type):
+	"""Calculate number of images in each chunk that could fit in memory
+
+	Inputs:
+		im_size: image size tuple
+		memory_size: memory size limit in MB
+		data_type: numpy data type that is used for saving image arrays
+
+	Output:
+		chunk_size: chunk size interger
+	"""
+	
+	# -- convert Mb to bytes
+	memory_size = 1048576 * memory_size
+	data_type_size = sp.array([0], dtype = data_type).itemsize
+	chunk_size =  memory_size / (im_size[0] * im_size[1] * \
+					data_type_size)
+
+	return chunk_size
+
+def main():
+
+	# -- parse argumnets
+	parser = argparse.ArgumentParser(description='Preapare image data in \
+		pickle files, divided in train, valid and test subset')
+	parser.add_argument('-i', '--input', dest = 'input', help =\
+		'path to folder that images are located', required = True)
+	parser.add_argument('-o', '--output', dest = 'output', help =\
+		'output path for pickle files', required = True)
+	parser.add_argument('-s', '--size', dest = 'im_size',
+		help = 'Size to change image to. One number reqired, \
+		will make images square', required = True, type = int)
+	parser.add_argument('-m', '--memsize', dest = 'mem_size',
+		help = 'GPU global memory size in Mb', default = 30, type = int)
+	parser.add_argument('-t', '--type', dest='data_type' , choices = [32, 64],
+		help = 'image array data types, 32: for floate32 \
+		64: for float64. (Many GPU\'s just support float32')
+	parser.add_argument('--train', dest = 'train', type = int, 
+		default = 50, help = '% of data for training')
+	parser.add_argument('--valid', dest = 'valid', type = int, 
+		default = 10, help = '% of data for training. \
+		Note: the rest would be for test')
+	parser.add_argument('-n', '--name', dest='name', default='data',
+		help = 'name pattern for output files')
+	parser.add_argument('-v', '--verbose', default = False,
+		action = 'store_true')
+	
+	args = parser.parse_args()
+
+	# -- initialize variables
+	global verbose
+	verbose = args.verbose
+
+	data_type = sp.float32
+	if args.data_type == 64:
+		data_type = sp.float64
+
+	# -- main
+
+	#get list of files
+	images, class_names, im_n_limit = get_image_list(args.input)
+	#dvidie data
+	train, valid, test = divide_data(images, im_n_limit, 
+							args.train, args.valid)
+	#chunk and save it
+	chunk =  chunk_size((args.im_size, args.im_size), args.mem_size, 
+						data_type)
+	chunk_data(train, chunk, (args.im_size, args.im_size),
+				args.output, args.name + '_train')
+	chunk_data(valid, chunk, (args.im_size, args.im_size),
+				args.output, args.name + '_valid' )
+	chunk_data(test, chunk, (args.im_size, args.im_size),
+				args.output, args.name + '_test')
+
+
+
 if __name__ == "__main__":
 	global verbose
 	verbose = False
 
 
-	# -- get images
-	img_path = '/idiap/home/mmirza/data/image_net/'
-	images, class_names, im_n_limit = get_image_list(img_path)
-	divide_data(images, im_n_limit, 50, 10)
+	#img_path = '/idiap/home/mmirza/data/image_net/'
+	#load_data(img_path)
+	main()
+
+
+	"""Example calculation of chunk sizes:
+		image size: 300 * 300
+		data tyoe: float32 -> 4 Byte
+		number of elements: 100
+
+		300 * 300 * 4 * 100 = 36000000 Bytes
+							= 34.33 Mb
+	"""
 	
